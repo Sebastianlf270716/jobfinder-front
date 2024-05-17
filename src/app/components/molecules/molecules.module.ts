c31bd3a94f2e8ea27fc9c47b9c2460e64c2231dd
Sebastian López Franco <sebastianlopezf2003@gmail.com>
2024-05-17 17:06:22 -0500

    Se realizo la barra de navegación el footer

--- src/app/components/molecules/molecules.module.ts
+++ src/app/components/molecules/molecules.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AtomsModule } from '../atoms/atoms.module';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     AtomsModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ]
 })
 export class MoleculesModule { }
