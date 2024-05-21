f5a52295ba9c37da0e3720f841dfb2ca3115196c
Megamanchi0 <menlockfull@gmail.com>
2024-05-20 21:25:47 -0500

    actualización - Formularios

--- src/app/components/pages/profile-consult-employer/profile-consult-employer.component.ts
+++ src/app/components/pages/profile-consult-employer/profile-consult-employer.component.ts
@@ -6,6 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-consult-employer.component.scss']
 })
 export class ProfileConsultEmployerComponent implements OnInit {
+  soloLectura: boolean = true;
+  visible: string = "";
+  invisible: string = "ocultar";
+
+  id: Number = 0;
+  nombre: string = "";
+  ciudad: string = "";
+  actividad: string = "";
+  correo: string = "";
+  descripcion: string = "";
+
+  public habilitarCampos(){
+    this.soloLectura = false;
+    this.visible = "ocultar";
+    this.invisible = "";
+  }
+
+  public bloquearCampos(){
+    location.reload()
+  }
 
   constructor() { }
 
